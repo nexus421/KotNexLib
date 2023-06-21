27d88c602b30b91c9fb662b0ae5bf6eb1e704356
Marvin <marvin.kickner@gmail.com>
2023-06-21 17:54:20 +0200

    First Push

---
+++ src/main/kotlin/GlobalFunctions.kt
@@ -0,0 +1,38 @@
+package bayern.kickner.kotlin_extensions_android
+
+import kotlin.system.measureTimeMillis
+
+/**
+ * Measures the time like measureTimeMillis. But this method can return anything including the execution time.
+ */
+fun <T> measureTimeMillisAndReturn(block: () -> T): ResultTimeMeasure<T> {
+    val result: T
+    val time = measureTimeMillis {
+        result = block()
+    }
+    return ResultTimeMeasure(result, time)
+}
+
+data class ResultTimeMeasure<T>(val result: T, val timeMillis: Long)
+
+/**
+ * Executes action as long as it returns not null.
+ * If action returns null, the callback onNull will be called.
+ *
+ * @return The first Object, which ist not null.
+ */
+inline fun <reified T> doUntilNotNull(noinline onNull: (() -> Unit)? = null, action: () -> T?): T {
+    while (true) {
+        action()?.let {
+            return it
+        } ?: onNull?.invoke()
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package bayern.kickner.kotlin_extensions_android
+
+fun main() {
+    println("Hello World")
+}
