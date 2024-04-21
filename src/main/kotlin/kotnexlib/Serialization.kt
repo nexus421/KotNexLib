ccdf78879c3d6ecd208571a32e0708442f04176b
Marvin <marvin@kickner.bayern>
2024-04-21 09:08:38 +0200

    Java-serialization with bytearray

--- src/main/kotlin/kotnexlib/Serialization.kt
+++ src/main/kotlin/kotnexlib/Serialization.kt
@@ -16,14 +16,39 @@ fun <T : Serializable> T.serializeToString(): String {
 }
 
 /**
+ * Converts any Object, that implements [Serializable], to its ByteArray representation
+ *
+ * @return ByteArray representation of this [Serializable]-object.
+ */
+fun <T : Serializable> T.serializeToByteArray(): ByteArray {
+    val baos = ByteArrayOutputStream()
+    return ObjectOutputStream(ByteArrayOutputStream()).use {
+        it.writeObject(this)
+        baos.toByteArray()
+    }
+}
+
+/**
  * Converts any Base64-String representation back to the original [Serializable]-object. You may created the String with [serializeToString].
  *
  * @return the deserialized object or null on any error.
  */
 inline fun <reified T : Serializable> String.deserializeFromStringOrNull(noinline onError: ((Throwable) -> Unit)? = null): T? {
     return tryOrNull(onError = onError) {
-        val bais = ByteArrayInputStream(fromBase64ToByteArray())
-        ObjectInputStream(bais).use {
+        ObjectInputStream(ByteArrayInputStream(fromBase64ToByteArray())).use {
+            it.readObject().safeCast<T>()
+        }
+    }
+}
+
+/**
+ * Converts any serialized ByteArray representation back to the original [Serializable]-object. You may created the String with [serializeToByteArray].
+ *
+ * @return the deserialized object or null on any error.
+ */
+inline fun <reified T : Serializable> ByteArray.deserializeFromByteArrayOrNull(noinline onError: ((Throwable) -> Unit)? = null): T? {
+    return tryOrNull(onError = onError) {
+        ObjectInputStream(inputStream()).use {
             it.readObject().safeCast<T>()
         }
     }
