448c1f8c6faf83d5b95ed3419f6315b2234fdba3
Marvin <marvin@kickner.bayern>
2024-06-16 11:58:06 +0200

    New extension to add days to a Date-object.

--- src/main/kotlin/kotnexlib/CalendarFunctions.kt
+++ src/main/kotlin/kotnexlib/CalendarFunctions.kt
@@ -8,6 +8,7 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.math.absoluteValue
 
 
 fun Calendar.setTimeMillis(time: Long): Calendar {
@@ -39,7 +40,7 @@ fun Calendar.year() = get(Calendar.YEAR)
  * Default is from 0-11.
  * Set [from1To12] to true for 1-12
  */
-fun Calendar.month(from1To12: Boolean = false) = get(Calendar.MONTH) + if(from1To12) 1 else 0
+fun Calendar.month(from1To12: Boolean = false) = get(Calendar.MONTH) + if (from1To12) 1 else 0
 
 /**
  * Month from 1 to 12
@@ -54,7 +55,7 @@ fun Calendar.dayOfYear() = get(Calendar.DAY_OF_YEAR)
  * Hour of day.
  * @param is24Hours true for 24h ([Calendar.HOUR_OF_DAY]) or false for 12h. 12h is from 0-11. See [Calendar.HOUR] for more.
  */
-fun Calendar.hourOfDay(is24Hours: Boolean = true) = get(if(is24Hours) Calendar.HOUR_OF_DAY else Calendar.HOUR)
+fun Calendar.hourOfDay(is24Hours: Boolean = true) = get(if (is24Hours) Calendar.HOUR_OF_DAY else Calendar.HOUR)
 fun Calendar.minute() = get(Calendar.MINUTE)
 
 fun Calendar.seconds() = get(Calendar.SECOND)
@@ -102,3 +103,15 @@ fun Calendar.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(toInstan
 fun Date.toLocalTime(): LocalTime = LocalTime.ofInstant(toInstant(), ZoneId.systemDefault())
 fun Date.toLocalDate(): LocalDate = LocalDateTime.ofInstant(toInstant(), ZoneId.systemDefault()).toLocalDate()
 fun Date.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(toInstant(), ZoneId.systemDefault())
+
+/**
+ * Adds or subtract [amount] days from the current time.
+ * There will not be created a new Date-object! The given will be manipulated!
+ *
+ * @param amount adds this amount of days to this Date object. If negative, the days will be subtracted
+ */
+fun Date.addDays(amount: Int): Date {
+    val milliseconds = TimeUnit.DAYS.toMillis(amount.absoluteValue.toLong())
+    if (amount < 0) time -= milliseconds else time += milliseconds
+    return this
+}
