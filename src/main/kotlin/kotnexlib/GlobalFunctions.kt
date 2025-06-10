040d4c0c14377e3e99f50c962921348e93d2b087
Marvin <marvin@kickner.bayern>
2025-06-10 18:26:32 +0200

    - Refactor `doUntilNotNull` for improved readability and efficiency. - Add `measureTimeSilent` utility function for execution time measurement.

--- src/main/kotlin/kotnexlib/GlobalFunctions.kt
+++ src/main/kotlin/kotnexlib/GlobalFunctions.kt
@@ -29,11 +29,27 @@ data class ResultTimeMeasure<T>(val result: T, val timeMillis: Long)
  */
 inline fun <reified T> doUntilNotNull(noinline onNull: (() -> Unit)? = null, action: () -> T?): T {
     while (true) {
-        action()?.let {
-            return it
-        } ?: onNull?.invoke()
+        val result = action()
+        if (result == null) onNull?.invoke() else return result
     }
 }
+
+/**
+ * Measures the execution time of the provided block of code and allows handling the elapsed time.
+ * By default, the elapsed time is printed to the console if no custom handler is provided.
+ *
+ * @param T The return type of the block of code to be measured.
+ * @param time A callback function invoked with the measured time in milliseconds. Defaults to printing the time.
+ * @param block The block of code whose execution time is to be measured.
+ * @return The result of the executed block of code.
+ */
+inline fun <reified T> measureTimeSilent(time: (Long) -> Unit = { println("Time: $it ms") }, block: () -> T): T {
+    val start = System.currentTimeMillis()
+    val result = block()
+    time(System.currentTimeMillis() - start)
+    return result
+}
+
 
 //inline fun <reified T> doUntil(condition: () -> Boolean, noinline onConditionNotFulfilled: (() -> Unit)? = null, action: () -> T?): T {
 //    while (condition()) {
