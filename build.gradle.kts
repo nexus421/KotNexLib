c6908e2714d8ddcfcfa8809294da16902ee1414d
marvin <github@kickner.bayern>
2026-01-06 12:05:24 +0100

    Update dependencies and plugin versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    id("org.jetbrains.dokka") version "1.9.20"
+    kotlin("jvm") version "2.3.0"
+    id("org.jetbrains.dokka") version "2.1.0"
     `maven-publish`
 }
 
@@ -13,20 +13,20 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation(kotlin("reflect"))
-    compileOnly("io.objectbox:objectbox-kotlin:4.3.0")
-    compileOnly("io.objectbox:objectbox-java:4.3.0")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    compileOnly("io.objectbox:objectbox-kotlin:5.0.1")
+    compileOnly("io.objectbox:objectbox-java:5.0.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     compileOnly("io.github.g0dkar:qrcode-kotlin:4.2.0")
     compileOnly("io.ktor:ktor-server-core:3.0.2")
 
     //Ktor Client
-    compileOnly("io.ktor:ktor-client-core:3.1.3")
-    compileOnly("io.ktor:ktor-client-cio:3.1.3")
-    compileOnly("io.ktor:ktor-client-content-negotiation:3.1.3")
-    compileOnly("io.ktor:ktor-serialization-kotlinx-json:3.1.3")
-    compileOnly("io.ktor:ktor-client-auth:3.1.3")
+    compileOnly("io.ktor:ktor-client-core:3.3.3")
+    compileOnly("io.ktor:ktor-client-cio:3.3.3")
+    compileOnly("io.ktor:ktor-client-content-negotiation:3.3.3")
+    compileOnly("io.ktor:ktor-serialization-kotlinx-json:3.3.3")
+    compileOnly("io.ktor:ktor-client-auth:3.3.3")
 }
 
 kotlin {
